32904984e24701c1f1cff15f15bfb69391674521
Hanif Robbani <hanif03@gmail.com>
2026-01-05 16:16:39 +0700

    02.02

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const port = process.env.PORT || 5000
 
 const app = express();
 
+app.get('/api/tasks', (req, res) => {
+    res.status(200).json({ message: 'Get All Tasks' });
+})
 
-app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Server listening on port ${port}!`))
