3e7189dbd5be3986eac1d75723e888ba68c03dbc
Hanif Robbani <hanif03@gmail.com>
2026-01-07 18:15:56 +0700

    03.01 connect

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,22 +1,23 @@
-const getTasks = (req, res) => {
-    res.status(200).json({ message: 'Get All Tasks' });
-}
+const asyncHandler = require("express-async-handler");
 
-const setTask = (req, res) => {
+const getTasks = asyncHandler( async (req, res) => {
+    res.status(200).json({ message: 'Get All Tasks' });
+})
+
+const setTask = asyncHandler( async (req, res) => {
     if(!req.body?.text){
         res.status(400)
         throw new Error('Please enter task')      
-    }
-    
-    
+    }   
     res.status(200).json({ message: 'Create Task' });
-}
+})
 
-const updateTask = (req, res) => {
+const updateTask = asyncHandler( async (req, res) => {
     res.status(200).json({ message: `Task ${req.params.id} updated` });
-}
-const deleteTask = (req, res) => {
+})
+
+const deleteTask = asyncHandler( async (req, res) => {
     res.status(200).json({ message: `Task ${req.params.id} deleted` });
-}
+})
 
 module.exports = { getTasks, setTask, updateTask, deleteTask }
