f90bfe3bdac1bc479e85d94cf144446588a7cb44
Hanif Robbani <hanif03@gmail.com>
2026-01-11 17:43:46 +0700

    07.01 add cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const dotenv = require('dotenv').config();
 const connectDB = require('./connect/database');
@@ -7,6 +8,7 @@ const port = process.env.PORT || 5000
 connectDB();
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
